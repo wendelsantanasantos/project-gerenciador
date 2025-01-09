bc73e29029066e4889511ae9a363bdb75fc20b3e
GustaLVS <168142217+GustaLVS@users.noreply.github.com>
2025-01-09 13:54:42 -0300

    Design Home

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -6,12 +6,20 @@ import { ReactComponent as Logo} from "../../img/logo-project-semFundo.svg";
 function Home(){
     return(
         <section className={styles.home_container}>
-            <h1>Bem-vindo ao <span>NEXU</span></h1>
-            <p>Começe a gerenciar seus projetos agora mesmo!</p>
-            <LinkButton para="/NovoProjeto" texto="Criar Projeto"/>
-            <Link to="/">
-          <Logo className={styles.logo}/>
-        </Link>
+            <div className={styles.text_section}>
+                <h1>TRABALHE <span>ONLINE</span></h1>
+                <h2>Trabalhe de Casa</h2>
+                <p>
+                    Descubra uma nova maneira de gerenciar seus projetos diretamente do conforto da sua casa. 
+                    Planeje, organize e execute projetos com eficiência e elegância.
+                </p>
+                <LinkButton para="/NovoProjeto" texto="Criar Projeto" />
+            </div>
+            <div className={styles.logo_section}>
+                <Link to="/">
+                    <Logo className={styles.logo} />
+                </Link>
+            </div>
         </section>
     )
 }
